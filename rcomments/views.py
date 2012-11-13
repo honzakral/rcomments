50a1c501dcfe6ea45d682c80d3023551dd26d098
Honza Kral <honza.kral@gmail.com>
2012-11-13 18:03:08 +0100

    refactor object fetching out of the view

--- rcomments/views.py
+++ rcomments/views.py
@@ -5,12 +5,15 @@ from django.http import Http404
 
 from rcomments.models import RComment
 
-def comment_list(request, ct_id, object_pk):
+def _get_obj(ct_id, object_pk):
     try:
         ct = ContentType.objects.get_for_id(int(ct_id))
-        obj = ct.get_object_for_this_type(pk=object_pk)
+        return ct.get_object_for_this_type(pk=object_pk)
     except (ContentType.DoesNotExist, ObjectDoesNotExist):
         raise Http404()
 
+def comment_list(request, ct_id, object_pk):
+    obj = _get_obj(ct_id, object_pk)
     clist = RComment.objects.for_model(obj)
     return TemplateResponse(request, 'comment_list.html', {'comment_list': clist})
+
